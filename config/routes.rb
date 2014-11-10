577e904b2e04b1d06ec4135aa13f2e5ccce840c8
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-11-10 15:39:25 -0500

    Created User Model, Controller, View

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
   root 'products#index'
 
 
   resources :products
+  resources :users, only: [:new, :create]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
